be0cce9ba86983f7525864fb713e72d8a6468882
Ethan Haynes <eghaynes17@gmail.com>
2019-12-15 17:04:48 -0600

    fix for tests and index.html validator

--- tests/test.py
+++ tests/test.py
@@ -1,83 +1,14 @@
-import urllib2, re
-import unittest
-from HTMLParser import HTMLParser
-
-# Dynamically generate dev URL.
-
-def generate_request_url():
-    url = ''
-    pat = re.compile("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
-
-    try:
-        # Get URL
-        response = urllib2.urlopen('url to dev ip')
-        # Get the code
-        content = response.read()
-        print "DEV ip: ", content
-        matches = pat.match(content)
-        if matches:
-            devip = matches.group(0)
-            url = "http://%s" % devip
-
-    except urllib2.HTTPError as e:
-        print e, 'while fetching ', url
-        return
-    return url
-
+import unittest, json, re
+from urllib import request, parse
+from html.parser import HTMLParser
 
 # function to get html data
 def get_html_data():
-    url = ''
-    try:
-        # Get URLs
-        url = generate_request_url()
-        response = urllib2.urlopen(url)
-        print "The URL is: ", response.geturl()
-        # Get the code
-        print "This gets the code: ", response.code
-        # Get all data
-        html = response.read()
-    except urllib2.HTTPError as e:
-        print e, 'while fetching', url
-        return
+    data = None
+    with open('index.html') as file:
+        data = file.read()
 
-    parser = MyHTMLParser()
-    parser.feed(html)
-    content = parser.data
-    parser.close()
-    return content
-
-
-# function to fetch status code
-def get_status_code():
-    try:
-        # Get URLs
-        url = generate_request_url()
-        response = urllib2.urlopen(url)
-        print "The URL is: ", response.geturl()
-        # Get the code
-        print "This gets the code: ", response.code
-        return response.code
-
-    except urllib2.HTTPError as e:
-        print e, 'while fetching', url
-        return
-
-
-# Unit Tests
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.response_code = 0
-        self.response_data = ''
-
-    def test_content(self):
-        self.response_data = get_html_data()
-        self.assertEqual(self.response_data, '')
-
-    def test_status_code(self):
-        self.response_code = get_status_code()
-        self.assertEqual(self.response_code, 200)
-
+    return data
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -85,16 +16,71 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.data = ''
+        self.img_count = 0
+        self.group_members = []
 
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
+        if tag == 'img':
+            # when we see an <img> increase count by 1
+            self.img_count += 1
 
     def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
+        # h1 tags are only used to display group member names
+        if tag == 'h1':
+            # remove all except space characters and alpha characters
+            data = re.sub(r'([^\s\w]|_)+', '', self.data)
+            # strip all spaces before and after alpha characters
+            data = data.strip()
+            # make all chars lowercase
+            data = data.lower()
+            # append to group_members list
+            self.group_members.append(data) 
 
     def handle_data(self, data):
-        print "Encountered some data  :", data
         self.data = data
+
+# Unit Tests
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.img_count = 3
+        self.group_members = ['ethan haynes', 'sanu ann abraham', 'jim depina']
+        self.messages = dict(messages=[])
+
+    def test_content_is_valid(self):
+        # get the data from our index.html 
+        body = get_html_data()
+        
+        # build the request object to validate html against w3 validator api
+        req = request.Request(
+            'https://validator.w3.org/nu/?out=json', 
+            data=body.encode('utf-8')
+        )
+        
+        # pass content type of html in header
+        req.add_header("Content-Type", "text/html")
+        
+        # make request
+        res = request.urlopen(req)
+        
+        # parse json response object
+        data = json.loads(res.read())
+        
+        # check that there aren't any errors
+        self.assertEqual(data, self.messages)
+
+    def test_html_content(self):
+        # get the data from our index.html 
+        body = get_html_data()
+        
+        # initialize our parser
+        parser = MyHTMLParser()
+        # pass in our index.html body string
+        parser.feed(body)
+
+        # check the <img> count is 3
+        self.assertEqual(parser.img_count, self.img_count)
+        # check that all of our names are present
+        self.assertEqual(parser.group_members, self.group_members)
 
 
 if __name__ == "__main__":
